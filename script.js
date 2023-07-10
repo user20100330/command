4ad4549ad7c481b13e14d33c671eecc839714198
user20100330 <134914842+user20100330@users.noreply.github.com>
2023-07-10 13:22:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function generate(){
   const player = document.getElementById("selector").value;
-  
+  const id = document.getElementById("itemid").value;
+  const amount = document.getElementById("ryou").value;
+  const display = document.getElementById("dis");
+  display.innerHTML = `/give ${player} ${id} ${amount}`
+
 }
